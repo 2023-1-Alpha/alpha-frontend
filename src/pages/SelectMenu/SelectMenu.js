452f4890e7665843432ece1e4119e4bdfde35356
JisuuungKim <cham0810@kookmin.ac.kr>
2023-05-19 19:22:29 +0900

    Add Modal

--- src/pages/SelectMenu/SelectMenu.js
+++ src/pages/SelectMenu/SelectMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import Modal from 'react-modal';
 import * as style from "./styles";
 import Sidebar from "../../components/Menu/Sidebar";
 import MenuHeader from "../../components/Menu/MenuHeader";
@@ -22,17 +23,41 @@ export default function SelectMenu() {
   console.log(menuType);
   console.log(menuTypeName);
 
+  const CustomStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      borderRadius: '20px',
+      width: '345px',
+      height: '345px',
+      transform:'translate(-50%, -50%)',
+      padding: '80px',
+      boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.25)',
+      border: '0',
+    }
+  }
+
   return (
-    <style.Wrap>
-      <Sidebar menuType={menuType} getMenutype={getMenutype} />
-      <style.Menu>
-        <SubTitleFont>
-          {menuTypeName}
-        </SubTitleFont>
-        <MenuHeader menuType={menuType} />
-        <MenuList menuType={menuType} />
-        <MenuFooter />
-      </style.Menu>
-    </style.Wrap>
+    <>
+      <Modal isOpen={true} style={CustomStyles}>
+        <div>
+          모달이다
+          <p>안뇽</p>
+        </div>
+      </Modal>
+      <style.Wrap>
+        <Sidebar menuType={menuType} getMenutype={getMenutype} />
+        <style.Menu>
+          <SubTitleFont>
+            {menuTypeName}
+          </SubTitleFont>
+          <MenuHeader menuType={menuType} />
+          <MenuList menuType={menuType} />
+          <MenuFooter />
+        </style.Menu>
+      </style.Wrap>
+    </>
   );
 }
