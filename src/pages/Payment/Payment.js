216d7908f8a95a3880f3dccdb0d8d6887fb30589
JisuuungKim <cham0810@kookmin.ac.kr>
2023-05-25 15:44:02 +0900

    Update Payment.js

---
+++ src/pages/Payment/Payment.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as style from "./styles";
+
+export default function Payment () {
+    return (
+        <style.Wrap>
+            
+        </style.Wrap>
+    )
+}
