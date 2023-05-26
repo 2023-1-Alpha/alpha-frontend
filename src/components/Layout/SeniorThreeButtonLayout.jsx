7616d926641161f6836cf64e01d4ec62857c5746
scy6500 <changyeop@seongchang-yeob-ui-MacBookPro.local>
2023-05-26 22:12:53 +0900

    feat: add senior page layout

---
+++ src/components/Layout/SeniorThreeButtonLayout.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import { SeniorBigButton, SeniorGoToHomeButton } from '../Button/SeniorButton';
+
+const Container = styled.div`
+  padding-top: 38px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LogoBlock = styled.img`
+  margin-bottom: 34px;
+  width: 200px;
+  height: 200px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 21px;
+  margin-bottom: 43px;
+`;
+
+export default function SeniorThreeButtonLayout({
+  firstBtnContent,
+  firstBtnOnclick,
+  secondBtnContent,
+  secondBtnOnclick,
+}) {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <LogoBlock
+        className="logoImg"
+        alt="로고 이미지"
+        src={process.env.PUBLIC_URL + '/Images/Main/Logo.svg'}
+      />
+      <ButtonContainer>
+        <SeniorBigButton onClick={firstBtnOnclick}>
+          {firstBtnContent}
+        </SeniorBigButton>
+        <SeniorBigButton onClick={secondBtnOnclick}>
+          {secondBtnContent}
+        </SeniorBigButton>
+      </ButtonContainer>
+      <SeniorGoToHomeButton
+        onClick={() => {
+          navigate('/seniorHome');
+        }}
+      />
+    </Container>
+  );
+}
