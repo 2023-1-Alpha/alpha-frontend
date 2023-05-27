38eb8ab3dd2a3360efa650783648be7dd8d23d72
VarGun <heegun4690@naver.com>
2023-05-27 22:58:38 +0900

    Delete MenuHeader

--- src/components/Menu/MenuHeader.js
+++ src/components/Menu/MenuHeader.js
@@ -1,58 +1,7 @@
-import React from "react";
-import WhiteFlatRoundedButton from "../Button/WhiteFlatRoundedButton";
-import * as style from "./styles";
+import React from 'react';
+import WhiteFlatRoundedButton from '../Button/WhiteFlatRoundedButton';
+import * as style from './styles';
 
 export default function MenuHeader(props) {
-  const burgersHeader = {
-    data: [
-      {
-        id: 1,
-        title: "전체",
-      },
-      {
-        id: 2,
-        title: "치킨",
-      },
-      {
-        id: 3,
-        title: "씨푸드",
-      },
-      {
-        id: 4,
-        title: "불고기",
-      },
-    ],
-  };
-
-  const dessertHeader = {
-    data: [
-      {
-        id: 1,
-        title: "전체",
-      },
-      {
-        id: 2,
-        title: "그 외 디저트",
-      },
-      {
-        id: 3,
-        title: "아이스크림",
-      },
-      {
-        id: 4,
-        title: "파이",
-      },
-    ],
-  };
-
-  return (
-    <style.MenuHeader>
-      {props.menuType === 1 &&
-        burgersHeader.data.map((item) => <WhiteFlatRoundedButton name={item.title} />
-        )}
-      {props.menuType === 2 &&
-        dessertHeader.data.map((item) =>  <WhiteFlatRoundedButton name={item.title} />
-        )}
-    </style.MenuHeader>
-  );
+  return <style.MenuHeader></style.MenuHeader>;
 }
