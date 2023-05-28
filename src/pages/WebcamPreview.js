257aeaaf147c2f9d822eee079fd707c4e3f2cbca
scy6500 <changyeop@comcom.ai>
2023-05-28 13:04:20 +0900

    feat: add modal

--- src/pages/WebcamPreview.js
+++ src/pages/WebcamPreview.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
   padding-top: 69px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: realtive;
 `;
 
 const Title = styled.span`
@@ -60,12 +62,24 @@ const InfoMessage = styled.span`
   text-align: center;
   color: #e93e18;
   margin-top: 30px;
+  ${(props) =>
+    props.small &&
+    `
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 27px;
+    margin-top: 0;
+    `}
 `;
 
 const ModalContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background: #f6f6f6;
+  background: rgba(0, 0, 0, 0.2);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const ModalBox = styled.div`
@@ -78,12 +92,37 @@ const ModalBox = styled.div`
   font-size: 24px;
   line-height: 36px;
   display: flex;
+  flex-direction: column;
   align-items: center;
   text-align: center;
   color: #121212;
+  width: 506px;
+  justify-content: center;
+  padding: 33px 0;
 `;
 
-const Modal = () => {
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 28px;
+  padding: 12px 0 21px 0;
+`;
+
+const Button = styled.button`
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: rgba(18, 18, 18, 0.7);
+  width: 160px;
+  height: 54px;
+  border: 1px solid rgba(18, 18, 18, 0.2);
+  border-radius: 40px;
+`;
+
+const ProgressingModal = () => {
   return (
     <ModalContainer>
       <ModalBox>
@@ -94,12 +133,50 @@ const Modal = () => {
   );
 };
 
+const ResultModal = () => {
+  const navigate = useNavigate();
+  return (
+    <ModalContainer>
+      <ModalBox>
+        고객님의 최적화 UI는 <br />
+        주문이 간편한 '간편모드'입니다.
+        <br />
+        <br />
+        간편 모드를 사용하시겠습니까?
+        <ButtonContainer>
+          {/* TODO: Page Connect */}
+          <Button
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            예
+          </Button>
+          <Button
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            아니요
+          </Button>
+        </ButtonContainer>
+        <InfoMessage small>
+          간편모드 사용 시 일부 메뉴 선택에 제한이 있을 수 있습니다
+        </InfoMessage>
+      </ModalBox>
+    </ModalContainer>
+  );
+};
+
 const WebcamPreview = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const overlayImageRef = useRef(null);
   const [capturedImage, setCapturedImage] = useState(null);
-  const [showModal, setShowModal] = useState(false);
+  const [showProgressingModal, setProgressingShowModal] = useState(false);
+  const [showResultModal, setResultShowModal] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const videoElement = videoRef.current;
@@ -143,7 +220,11 @@ const WebcamPreview = () => {
       });
   }, []);
 
-  const captureImage = () => {
+  useEffect(() => {
+    console.log(capturedImage);
+  }, [capturedImage]);
+
+  const captureImage = async () => {
     const canvasElement = canvasRef.current;
     const context = canvasElement.getContext('2d');
     context.drawImage(
@@ -156,11 +237,26 @@ const WebcamPreview = () => {
 
     const image = canvasElement.toDataURL('image/png');
     setCapturedImage(image);
+    setProgressingShowModal(true);
+    // TODO: API Integration
+    // const result = await fetch(API)
+    // const result = 'senior';
+    // // const result = "young"
+    // if (result === 'young') {
+    //   // TODO: Page Connect
+    //   navigate('/');
+    // } else if (result === 'senior') {
+    //   setProgressingShowModal(false);
+    //   setResultShowModal(true);
+    // } else {
+    //   // people not found
+    // }
   };
 
   return (
     <Container>
-      {/* {Modal()} */}
+      {showProgressingModal && ProgressingModal()}
+      {showResultModal && ResultModal()}
       <Title>가이드라인에 맞춰 촬영해주세요</Title>
       <VideoContainer>
         <video
